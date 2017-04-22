17a14ed7a68fd9ef881852a3ae0cf3f24e4798eb
Reza Pourmodheji <Reza@Rezas-MBP.fios-router.home>
2017-04-22 18:52:44 -0400

    Assignment Week 3 - update

--- cachematrix.R
+++ cachematrix.R
@@ -8,15 +8,19 @@
 ## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function (y){
-    x <<- y
-    inv <<- NULL
+    inv <- NULL        ##initialize the variable which update the
+                       ##cached inverse
+    set <- function (y){ #the sub-function which update the matrix in cache
+    x <<- y           ##update the matrix in cache
+    inv <<- NULL      ##when the matrix is updated the inverse should
+                      ##be updated (later)
   }
-  get <- function() x
-  setinv <- function (inverse) inv <<- inverse
-  getinv <- function() inv
-  list (set = set , get = get ,
+  get <- function() x ##this sub-function will return the matrix
+  setinv <- function (inverse) inv <<- inverse ##this sub-function
+                                               ##updates the cached
+                                               ##inverse
+  getinv <- function() inv ##this sub-function will return the inverse
+  list (set = set , get = get , ##four sub-function will be returned
         setinv = setinv , getinv=getinv)
 }
 
@@ -25,14 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## matrix returned by "makeCacheMatrix" above
 
 cacheSolve <- function(x, ...) {
-  inv <- x$getinv()
-  if (!is.null(inv)){
+  inv <- x$getinv()      ##retrive the cached inverse (updated or not)
+  if (!is.null(inv)){    ##check if the cached has been already calculated
     message("getting cached data")
-    return(inv)
+    return(inv)          ##returns the inverse and end the function
   }
-  newmatrix <- x$get()
-  inv <- solve(newmatrix,...)
-  x$setinv(inv)
+  newmatrix <- x$get()   ##needs to calculate the inverse therefore
+                         ##it needs to know the new matrix!
+  inv <- solve(newmatrix,...) #calculate the inverse of the new matrix
+  x$setinv(inv)          ##update the cached inverse
   inv
         ## Return a matrix that is the inverse of 'x'
 }
